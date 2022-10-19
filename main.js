f60de7e29afee8d2d7aa6d8f9edb63e2aff18f98
Emma Velarde <emma14.05.00zuliova@gmail.com>
2022-10-19 14:07:44 -0500

    Carrito de compras

--- main.js
+++ main.js
@@ -2,9 +2,32 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurguerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const carMenuIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click', () => togglepMenu(desktopMenu));
-menuBurguerIcon.addEventListener('click', () => togglepMenu(mobileMenu));
+menuEmail.addEventListener('click', () => {
+    if (!aside.classList.contains('inactive')) {
+        togglepMenu(aside)
+    }
+    togglepMenu(desktopMenu)
+});
+
+menuBurguerIcon.addEventListener('click', () => {
+    if (!aside.classList.contains('inactive')) {
+        togglepMenu(aside)
+    }
+    togglepMenu(mobileMenu)
+});
+
+carMenuIcon.addEventListener('click', () => {
+    if (!desktopMenu.classList.contains('inactive')) {
+        togglepMenu(desktopMenu)
+    }
+    if (!mobileMenu.classList.contains('inactive')) {
+        togglepMenu(mobileMenu)
+    }
+    togglepMenu(aside)
+});
 
 
 function togglepMenu(dropDownMenu) {
