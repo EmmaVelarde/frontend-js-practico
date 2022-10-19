727e7b783ca900fa9548b58b834013cc720ca8f6
Emma Velarde <emma14.05.00zuliova@gmail.com>
2022-10-19 13:17:23 -0500

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurguerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
+menuEmail.addEventListener('click', () => togglepMenu(desktopMenu));
+menuBurguerIcon.addEventListener('click', () => togglepMenu(mobileMenu));
 
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+
+function togglepMenu(dropDownMenu) {
+    dropDownMenu.classList.toggle('inactive');
 }
+
